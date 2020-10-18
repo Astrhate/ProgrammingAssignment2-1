0cc471df79c64067f0cf294c1f8efc0764e261a7
Astrhate <davisuf@gmail.com>
2020-10-18 17:49:07 -0500

    Commit with changes to make a functional function

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+##set Matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  #var ans will be the inverse value
+  ans <- NULL #sets value to null
+  
+  set <- function(y){
+    x <<- y
+    j <<- NULL
+  }
+  #get matrix
+  get <- function()x
+  #set the inverse function
+  setans <- function(inverse) ans <<- inverse
+  #getting the inverse 
+  getans <- function() ans 
+  list(set = set, get = get, 
+       setans = setans, 
+       getans = getans)
 }
 
-
-## Write a short comment describing this function
-
+#Getting the inverse of the original matrix inptu in 'x'
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ans <- x$getans()
+  #if inverse has already been calculated
+  if(!is.null(ans)){
+    #skips the calculation by getting from cache
+    message("getting cached data")
+    return(ans)
+  }
+  #if not cached it calculates the inverse
+  mat <- x$get()
+  ans <- solve(mat, ...)
+  #sets the value of the inverse in the cache
+  x$setans(ans)
+  return(ans)
+  
 }
